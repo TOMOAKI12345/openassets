510e75949b5cef979c34475edfe8acb496a236bb
TOMOAKI12345 <tomoaki12345@gmail.com>
2015-08-18 19:18:35 +0900

    fix some of the error, however mocha test failed

--- test/ColoringEngine.js
+++ test/ColoringEngine.js
@@ -14,8 +14,7 @@ var assert = require('assert'),
     OutputType        = require('../lib/protocol/OutputType'),
     TransactionOutput = require('../lib/protocol/TransactionOutput'),
     bitcore = require('bitcore'),
-    RpcClient = require('bitcoind-rpc'),
-    EncodedData = require('bitcore/util/EncodedData');
+    RpcClient = require('bitcoind-rpc')
 
 
 describe("TransactionOutput", function () {
